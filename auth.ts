8a7fe390d53a2f231adf776f2dbdf62885e89834
Gewall <alginugraha21@gmail.com>
2024-11-27 23:28:29 +0700

    update

--- auth.ts
+++ auth.ts
@@ -9,8 +9,8 @@ import GitHub from "next-auth/providers/github"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   adapter:SupabaseAdapter({
-    secret:process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    url:process.env.SUPABASE_URL!
+    secret:process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+    url:process.env.SUPABASE_URL as string
   }),
   debug:true
 })
